84beb30573fe8065019bf4c0b6ad980db645667c
anamolhasan <mdanamolhasan1998@gmail.com>
2025-08-28 21:49:47 +0600

    context api added

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import './index.css'
 import { RouterProvider } from "react-router/dom";
 
 import { router } from './router/Router.jsx';
+import AuthProvider from './context/AuthProvider.jsx';
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />,
+    <AuthProvider >
+        <RouterProvider router={router} />,
+    </AuthProvider>
   </StrictMode>,
 )
