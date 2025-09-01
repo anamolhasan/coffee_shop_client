be89a46315f93f48a89befa9e9922742eb792fdb
anamolhasan <mdanamolhasan1998@gmail.com>
2025-09-02 00:28:52 +0600

    tenstack query practis

--- src/page/users/Users2.jsx
+++ src/page/users/Users2.jsx
@@ -1,9 +1,131 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+import Loading from "../../components/Loading";
 
 const Users2 = () => {
-  return (
-    <div>Users2</div>
-  )
-}
 
-export default Users2
+
+    const {data:users, isPending, isError, error} = useQuery({
+        queryKey:['users'],
+        queryFn: async () => {
+            const res = await fetch(`${import.meta.env.VITE_API_URL}/users`)
+            return res.json()
+        }
+    })
+
+    if(isPending) return <Loading />
+    if(isError) return <p>{error.message}</p>
+
+//   const [users, setUsers] =useState([])
+
+//   useEffect(()=>{
+//     fetch(`${import.meta.env.VITE_API_URL}/users`)
+//       .then(res => res.json())
+//       .then(data => {
+//         setUsers(data)
+//       })
+//   },[])
+
+
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // 🔹 Step 1: First delete form database
+        fetch(`${import.meta.env.VITE_API_URL}/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              // Then delete form firebase (only current user)
+            //   deleteAccountUser()
+            //     .then(() => {
+            //       // delete filter
+            //       const remainingUser = users.filter((user) => user._id !== id);
+            //       setUsers(remainingUser);
+
+            //       Swal.fire({
+            //         title: "Deleted!",
+            //         text: "Your file has been deleted.",
+            //         icon: "success",
+            //       });
+            //     })
+            //     .catch((error) => {
+            //       console.error("Firebase delete error:", error.message);
+            //     });
+            }
+          });
+      }
+    });
+  };
+  return (
+    <div>
+      <div>
+        <h2 className="text-3xl">Users: {users.length}</h2>
+
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              {users.map((user, index) => (
+                <tr key={user._id}>
+                  <th>{index + 1}</th>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img
+                            src={user.photo}
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold">{user.name}</div>
+                        <div className="text-sm opacity-50">{user.address}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{user.phone}</td>
+                  <td>{user.email}</td>
+                  <th>
+                    <button className="btn btn-xs">V</button>
+                    <button className="btn btn-xs">E</button>
+                    <button
+                      onClick={() => handleDelete(user._id)}
+                      className="btn btn-xs"
+                    >
+                      X
+                    </button>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Users2;
